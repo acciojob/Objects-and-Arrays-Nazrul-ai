2edc115a19fcc596bb6ebff9008d41b6823e855b
Nazrul-ai <68251918+Nazrul-ai@users.noreply.github.com>
2023-09-24 01:28:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,16 @@ const person = {
   name: "John Doe",
   age: 80,
 };
+// const team1;
+// for(let i = 0; i < players.length; i++){
+// 	players[i] = team1[i];
+// }
+// con
+// const len = players.length;
+// const team1 = players.slice(0, len);
 
+// const team2 = players;
+// const cap1 = person;
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
 
 window.players = players;
